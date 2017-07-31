aaf32b87a588050f39ddab70242b7be6b88fe86f
Siphesihle Dube <2022746@TRUWORTHS.CO.ZA>
2017-07-31 14:35:31 +0200

    Completed Tutorial

---
+++ AspNetWebApi2/AspNetWebApi2/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using AspNetWebApi2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetWebApi2.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[] {
+            new Product { Id = 1, Name = "Tomato Soup", Categoty = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Categoty = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Categoty = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts() {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id) {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ AspNetWebApi2/AspNetWebApi2/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetWebApi2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Categoty { get; set; }
+        public decimal Price { get; set; }
+    }
+}
